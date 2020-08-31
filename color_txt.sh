cf41632809dcb2c90ea033c535d57360af9d56e6
windvalley <i@sre.im>
2020-08-31 10:19:57 +0800

    Add color_txt.sh

---
+++ color_txt.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# color_txt.sh
+
+
+color(){
+    local role=$1
+    shift
+    local str="$*"
+
+    case $role in
+        success|ok)
+            color=[32m
+            ;;
+        failure|fail)
+            color=[31m
+            ;;
+        warning|warn)
+            color=[33m
+            ;;
+        *)
+            color=[39m  # default white color
+            ;;
+    esac
+
+    echo -e "\033$color$str\033[m"
+}
+
+
+# e.g.
+echo -n "mysql started "
+color success "[OK]"
+
+color ok [OK]
+
+color fail error found
+
+color warn warning
+
+color other hello world
